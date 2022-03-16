b7c3bb9ce5762df85e9a728bd9a2ec8834c681d8
ihalangel <ihalangel@gmail.com>
2022-03-16 15:15:41 -0400

    add compiles

--- test/Horses.js
+++ test/Horses.js
@@ -1,6 +1,7 @@
-const Horse = artifacts.require('Horse');
+const Horses = artifacts.require("Horses");
 
-contract('Horse', accounts => (
+
+contract('Horses', accounts => (
     // [alice, bob]=accounts; 
     // console.log('Alice:'.alice);
     console.log(accounts[0])
